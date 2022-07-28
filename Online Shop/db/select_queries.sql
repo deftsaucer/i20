6279a52a91a69942f5d9bcb1673482548da88662
deftsaucer <ss.tru.main@gmail.com>
2022-07-28 21:53:44 +0700

    corrections

--- Online Shop/db/select_queries.sql
+++ Online Shop/db/select_queries.sql
@@ -9,18 +9,25 @@ having count > 0
 order by count desc;
 
 
+
 /*старница раздела с товарами*/
-select c.category_name, c.category_description, p.name, ph.link, ph.alt
+select c.category_name, c.category_description
+from categories as c
+where c.id = 4;
+
+select p.name, p.main_category_id, ph.link, ph.alt
 from products as p 
 	join categoryproducts as cp on cp.product_id = p.id 
 	join categories as c on c.id = cp.category_id 
 	join photos as ph on ph.id = p.main_photo_id
-where c.id = 4; 
+where c.id = 4
+limit 12, 12;
+ 
 
 
 /*страница товара*/
-select p.id, p.name, p.main_photo_id, p.price, p.new_price, p.sale_price, p.description
-from products as p;
+select * from products
+where id = 11;
 
 select c.id, c.category_name
 from products as p
