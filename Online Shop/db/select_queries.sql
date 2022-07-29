c4517d165f1c165e87634bf9b29852d8643b201d
deftsaucer <ss.tru.main@gmail.com>
2022-07-29 22:25:16 +0700

    Update select_queries.sql

--- Online Shop/db/select_queries.sql
+++ Online Shop/db/select_queries.sql
@@ -5,7 +5,6 @@ from categories as c
 	join products as p on p.id = cp.product_id
 where p.active_status = 1
 group by c.id
-having count > 0
 order by count desc;
 
 
@@ -15,13 +14,13 @@ select c.category_name, c.category_description
 from categories as c
 where c.id = 4;
 
-select p.name, c.category_id, ph.link, ph.alt
+select p.name, c.category_name, ph.link, ph.alt
 from products as p 
 	join categoryproducts as cp on cp.product_id = p.id 
 	join categories as c on c.id = cp.category_id 
 	join photos as ph on ph.id = p.main_photo_id
 where c.id = 4
-limit 12, 12;
+limit 0, 12;
  
 
 
@@ -30,13 +29,11 @@ select * from products
 where id = 11;
 
 select c.id, c.category_name
-from products as p
-	join categoryproducts as cp on p.id = cp.product_id
+from categoryproducts as cp
 	join categories as c on c.id = cp.category_id
-where p.id = 5;
+where cp.product_id = 5;
 
 select ph.id, ph.link, ph.alt
-from products as p
-	join productphotos as pph on p.id = pph.product_id
+from productphotos as pph
 	join photos as ph on ph.id = pph.photo_id
-where p.id = 5;
+where pph.product_id = 5;
