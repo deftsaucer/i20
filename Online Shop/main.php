b0f7f53683071bf6aea9f290899376d2580f04a1
deftsaucer <ss.tru.main@gmail.com>
2022-07-29 14:22:10 +0700

    html

---
+++ Online Shop/main.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="style_list.css">
+  <title>Интернет магазин</title>
+</head>
+<body>
+  <div class="container">
+    <header class="header">
+      <a href="#" class="header_logo"><h1>Интернет-магазин</h1></a>
+      <nav>
+        <a href="#" class="nav_link">На главную</a>
+        <a href="#" class="nav_link">Обратная связь</a>
+      </nav>
+    </header>
+
+    <button type="button" name="return" class="return_button">
+      <img class="return_button_img" src="images/arrow.png" alt="Назад">
+    </button>
+
+    <h1 class="page-title">Категрии</h1>
+
+    <div class="content">
+      <div class="item">
+        <figure>
+          <img class="item_img" src="images/cat_img.png" alt="image">
+          <a href="#" class="item_name">category_name</a>
+        <figure>
+      </div>
+
+      <div class="item">
+        <figure>
+          <img class="item_img" src="images/cat_img.png" alt="image">
+          <a href="#" class="item_name">category_name</a>
+        <figure>
+      </div>
+
+      <div class="item">
+        <figure>
+          <img class="item_img" src="images/cat_img.png" alt="image">
+          <a href="#" class="item_name">category_name</a>
+        <figure>
+      </div>
+
+      <div class="item">
+        <figure>
+          <img class="item_img" src="images/cat_img.png" alt="image">
+          <a href="#" class="item_name">category_name</a>
+        <figure>
+      </div>
+
+      <div class="item">
+        <figure>
+          <img class="item_img" src="images/cat_img.png" alt="image">
+          <a href="#" class="item_name">category_name</a>
+        <figure>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
